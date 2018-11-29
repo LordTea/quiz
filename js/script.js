d973da1f2a0539551fb599dba83e5bd95c007574
Thienan Nguyen <thienan.nguyen@ousd.us>
2018-11-29 01:58:10 +0000

    Changed image, added new author, changed text, started on the javscript

--- js/script.js
+++ js/script.js
@@ -3,12 +3,33 @@
 $(document).ready(function() {
     $("button").click(function() {
         var name = $("#name").val();
-        var techPlacement = "TBD";
+        var badQuiz = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
         var q1Result = $("#question1").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
+        var q4Result = $("#question4").val();
         var totalScore;
     });
-
 });
+
+function scoreQ1(name){
+    if ((name).toUpperCase().trim() === "BEN SHAPIRO"){
+        return 100;
+        
+    } else {
+        return -50;
+    }
+}
+
+function scoreQ2(num){
+    if (num <= 10 && num >= 1){
+        return (50 - (num*10));
+    } else {
+        return -100;
+    }
+}
+
+function calculateScore(a, b, c, d, e){
+    return (a+b+c+d+e);
+}
